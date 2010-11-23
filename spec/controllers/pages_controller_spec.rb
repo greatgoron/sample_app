ca9dad24ca67413b2551b47d4bf778784f49923d
Arturo Moreno Arriaga <greatgoron@gmail.com>
2010-11-22 19:19:40 -0600

    Help page added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,29 +8,24 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
-  end
-  
-  describe "GET 'home'" do
-    it "should be successful" do
+    
+    it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
-
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
-  end
-  
-  describe "GET 'contact'" do
-    it "should be successful" do
+    
+    it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
+      
   end
   
   describe "GET 'about'" do
@@ -38,13 +33,25 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
-
-  describe "GET 'about'" do
-    it "should be successful" do
+    
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title",
                         :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_successful
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  
+  
 end
