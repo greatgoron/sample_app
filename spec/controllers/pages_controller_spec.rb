6d939da2ca4e03ae5b272e168ebfc0c86cca6015
Arturo Moreno Arriaga <greatgoron@gmail.com>
2010-11-22 19:03:53 -0600

    Donde with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+  end
+  
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -15,5 +24,27 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
